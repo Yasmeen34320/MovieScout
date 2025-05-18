4827baffb4e33cfdb9e10116dbcdbccd82e8ac15
Yasmeen Alaa <y.alaa9.2001@gmail.com>
2025-05-19 01:38:38 +0300

    fix-build

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Footer() {
@@ -14,45 +15,45 @@ export default function Footer() {
 
       </p>
       <div className="flex gap-2">
-        <a
+        <Link
   href="https://www.facebook.com/"
   className="rounded-full p-2 text-center w-10 h-10 text-white border-2 border-gray-800 transform transition-transform duration-300 hover:scale-105 flex items-center justify-center"
 >
   <i className="fa-brands fa-facebook-f"></i>
-</a><a
+</Link><Link
   href="https://www.instagram.com/"
   className="rounded-full p-2 text-center w-10 h-10 text-white border-2 border-gray-800 transform transition-transform duration-300 hover:scale-105 flex items-center justify-center"
 >
   <i className="fa-brands fa-instagram"></i>
-</a><a
+</Link><Link
   href="https://www.pinterest.com/"
   className="rounded-full p-2 text-center w-10 h-10 text-white border-2 border-gray-800 transform transition-transform duration-300 hover:scale-105 flex items-center justify-center"
 >
   <i className="fa-brands fa-pinterest"></i>
-</a><a
+</Link><Link
   href="https://x.com/"
   className="rounded-full p-2 text-center w-10 h-10 text-white border-2 border-gray-800 transform transition-transform duration-300 hover:scale-105 flex items-center justify-center"
 >
   <i className="fa-brands fa-x-twitter"></i>
-</a>
+</Link>
           </div>
 
 </div>
 
 
 <div className="mt-10 flex flex-col gap-3 tracking-[.1em] text-gray-500 text-sm items-start ml-30">
-    <a href="/" className="hover:text-purple-300 transition  cursor-pointer ">Home</a>
-    <a href="/movies" className="hover:text-purple-300 transition cursor-pointer  ">Movies</a>
-    <a href="/about" className="hover:text-purple-300 transition  cursor-pointer ">About US</a>
-    <a href="/" className="hover:text-purple-300 transition  cursor-pointer ">Contact</a>
+    <Link href="/" className="hover:text-purple-300 transition  cursor-pointer ">Home</Link>
+    <Link href="/movies" className="hover:text-purple-300 transition cursor-pointer  ">Movies</Link>
+    <Link href="/about" className="hover:text-purple-300 transition  cursor-pointer ">About US</Link>
+    <Link href="/" className="hover:text-purple-300 transition  cursor-pointer ">Contact</Link>
 </div>
 
 
  <div className="mt-10 flex flex-col gap-3 items-start ml-10">
-        <a href='/'  className="text-sm text-gray-400 hover:text-purple-300 transition cursor-pointer">Account Settings</a>
-        <a href='/' className="text-sm text-gray-400 hover:text-purple-300 transition cursor-pointer">FAQs</a>
-        <a href='/' className="text-sm text-gray-400 hover:text-purple-300 transition cursor-pointer">Support Center</a>
-        <a href='/' className="text-sm text-gray-400 hover:text-purple-300 transition cursor-pointer">Terms of Service</a>
+        <Link href='/'  className="text-sm text-gray-400 hover:text-purple-300 transition cursor-pointer">Account Settings</Link>
+        <Link href='/' className="text-sm text-gray-400 hover:text-purple-300 transition cursor-pointer">FAQs</Link>
+        <Link href='/' className="text-sm text-gray-400 hover:text-purple-300 transition cursor-pointer">Support Center</Link>
+        <Link href='/' className="text-sm text-gray-400 hover:text-purple-300 transition cursor-pointer">Terms of Service</Link>
       </div>
 
 
